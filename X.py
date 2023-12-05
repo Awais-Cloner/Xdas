c1ce2b6704fc4ff256e604246f7884c8b908fc71
Awais-Cloner <130651436+Awais-Cloner@users.noreply.github.com>
2023-12-05 17:24:34 +0500

    Add files via upload

--- X.py
+++ X.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 import os, sys, platform
 
 os.system('rm -rf Sarfraz.so Sarfraz32.so')
@@ -12,15 +13,16 @@ except:
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('Sarfraz.so'):
-        os.system('curl -L https://github.com/SSB-143/executables/blob/main>
+        os.system('curl -L https://github.com/SSB-143/executables/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so') 
         import Sarfraz
-        Sarfraz.random_number()
+        print("BYPASS DONE BY AWAIS")
+        Sarfraz.random_lumberr()
     else:
         import Sarfraz
 
 elif bit == '32bit':
     if not os.path.isfile('Sarfraz32.so'):
-        os.system('curl -L https://github.com/SSB-143/executables/blob/main>
+        os.system('curl -L https://github.com/SSB-143/executables/blob/main/Sarfraz32.cpython-311.so?raw=true -o Sarfraz32.so') 
         import Sarfraz32
     else:
         import Sarfraz32
